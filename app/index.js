68de61b6ad5be409df2d18fa0372ec07c1a959f9
Denis P <6231990@gmail.com>
2025-02-17 09:53:32 +0300

    fix form data parse

--- app/index.js
+++ app/index.js
@@ -21,6 +21,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/api', echoRouter);
